4442dde2cd3310fff1b286bdc0472465ee083c63
Valerie Yew <48005126+valerieyew@users.noreply.github.com>
2023-10-17 07:25:16 +0800

    fixed next installation and testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -26,12 +26,12 @@ const Testimonials = () => {
                                 <path d='M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z' />
                             </svg>
                             <p className='leading-relaxed mb-6'>
-                                I use headphones for long hours in my office.
                                 After I started bringing emGuarde to work, there
-                                was a distinctive increase in my energy level
-                                during and after work. Now I'm increasingly able
-                                to concentrate on my work even without my
-                                headphones.
+                                was a distinctive increase in my overall energy
+                                level during and after work. Now I'm
+                                increasingly able to concentrate on my work for
+                                longer hours, sometimes without my headphones
+                                too.
                             </p>
                             <p>
                                 <b className='title-font text-gray-900 font-heading'>
@@ -52,7 +52,7 @@ const Testimonials = () => {
                                 <path d='M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z' />
                             </svg>
                             <p className='leading-relaxed mb-6'>
-                                testing Staying up late to study and being surrounded by
+                                Staying up late to study and being surrounded by
                                 gadgets was taking a toll on my well-being.
                                 Since emGuarde entered my life, I&apos;ve
                                 noticed a significant improvement in my focus
@@ -118,15 +118,14 @@ const Testimonials = () => {
                                 <path d='M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z' />
                             </svg>
                             <p className='leading-relaxed mb-6'>
-                                Finding inner balance amid our fast-paced,
-                                tech-savvy lives is a challenge. After
-                                installing emGuarde in my studio, I can teach my
-                                yoga classes without being affected by
-                                electromagnetic disruptions. My students and I
-                                have experienced a deeper connection to our
-                                practice, allowing us to find serenity even with
-                                all the devices and radio waves surrounding our
-                                environment.
+                                My overall health is extremely important to me.
+                                When I heard of emGuarde being able to eliminate
+                                radiation from my phone, I immediately went to
+                                find out more. Two weeks into using emGuarde, I
+                                can actually feel the reduction of external
+                                stress on my mind and body. It's easier
+                                for me now to find my inner peace and do my
+                                meditation.
                             </p>
                             <p>
                                 <b className='title-font text-gray-900 font-heading'>
