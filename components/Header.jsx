2d787c154033baf99a4d9df4334b7aaccf6314b3
Valerie Yew <48005126+valerieyew@users.noreply.github.com>
2023-11-13 21:37:46 +0800

    initial code for navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,64 +1,96 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Menu } from "@headlessui/react";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
-const nav = [
-  { name: "Health Benefits", href: "#health-benefits" },
-  { name: "Specifications", href: "#specification" },
-  { name: "Testimonials", href: "#testimonials" },
-  { name: "Events", href: "#events" },
-  { name: "Buy Now", href: "#buy-section" },
-];
+import Link from 'next/link';
 
 const Header = () => {
-  return (
-    <header className="bg-black">
-      <div className="mx-auto max-w-7xl px-5 flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <Image alt="logo" src={"/logo.png"} width={50} height={50} />
-          <h5 className=" py-5 font-heading text-lg text-white font-semibold">
-            Kangen Wiz Global (emGuarde)
-          </h5>
-        </div>
-        <div>
-          <ul className="hidden md:flex gap-5 ">
-            {nav.map((link) => (
-              <li key={link.name}>
-                <Link href={link.href} className="text-white font-heading px-5">
-                  {link.name}
+    return (
+        <nav class='bg-black z-10'>
+            <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+                <Link
+                    href='https://www.emguardesingapore.com/'
+                    class='flex items-center space-x-3 rtl:space-x-reverse'
+                >
+                    <img
+                        src='/logo.png'
+                        class='h-8'
+                        alt='Kangen Wiz Global Logo'
+                    />
+                    <span class='self-center font-semibold text-white'>
+                        Kangen Wiz Global (emGuarde)
+                    </span>
                 </Link>
-              </li>
-            ))}
-          </ul>
-
-          <div className="md:hidden">
-            <Menu>
-              <Menu.Button>
-                <HiOutlineMenuAlt3 color="white" size={25} />
-              </Menu.Button>
-              <Menu.Items
-                className={
-                  "absolute top-[68px] left-0 bg-black grid w-full h-auto py-5"
-                }
-              >
-                <ul className="grid gap-3">
-                  {nav.map((link) => (
-                    <li key={link.name}>
-                      <Link
-                        href={link.href}
-                        className="text-white font-heading px-5 py-5"
-                      >
-                        {link.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </Menu.Items>
-            </Menu>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+                <button
+                    data-collapse-toggle='navbar-default'
+                    type='button'
+                    class='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden'
+                    aria-controls='navbar-default'
+                    aria-expanded='false'
+                >
+                    <span class='sr-only'>Open main menu</span>
+                    <svg
+                        class='w-5 h-5'
+                        aria-hidden='true'
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 17 14'
+                    >
+                        <path
+                            stroke='currentColor'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            stroke-width='2'
+                            d='M1 1h15M1 7h15M1 13h15'
+                        />
+                    </svg>
+                </button>
+                <div
+                    class='hidden w-full md:block md:w-auto'
+                    id='navbar-default'
+                >
+                    <ul class='font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0'>
+                        <li>
+                            <Link
+                                href='#health-benefits'
+                                class='block py-2 px-3 text-white rounded md:hover:text-yellow-500 md:p-0'
+                                aria-current='page'
+                            >
+                                Health Benefits
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='#testimonials'
+                                class='block py-2 px-3 text-white rounded md:hover:text-yellow-500 md:p-0'
+                            >
+                                Testimonials
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='#specification'
+                                class='block py-2 px-3 text-white rounded md:hover:text-yellow-500 md:p-0'
+                            >
+                                Specifications
+                            </Link>
+                        </li>
+                        {/* <li>
+                            <Link
+                                href='#events'
+                                class='block py-2 px-3 text-white rounded md:hover:text-yellow-500 md:p-0'
+                            >
+                                Events
+                            </Link>
+                        </li> */}
+                        <li>
+                            <Link
+                                href='#buy-section'
+                                class='block py-2 px-3 text-white rounded md:hover:text-yellow-500 md:p-0'
+                            >
+                                Buy Now
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 export default Header;
--- pages/index.jsx
+++ pages/index.jsx
@@ -25,6 +25,7 @@ export default function Home() {
         <Specification />
         <Coverage />
         <Video />
+        {/* <JoinOurEvent /> */}
         <Buy />
         <Footer />
       </main>
