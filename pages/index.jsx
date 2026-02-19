d1f67d521f8837b4150222f56ee2d03276d3a1f7
Valerie Yew <48005126+valerieyew@users.noreply.github.com>
2026-02-19 14:53:33 +0800

    file rename

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,57 @@
-import SEO from "../components/SEO";
-import CtaBanner from "../components/CtaBanner";
-import WelcomeBanner from "../components/WelcomeBanner";
-import PortableFeatureBanner from "../components/PortableFeatureBanner";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import RadiationIntroBanner from "../components/RadiationIntroBanner";
-import CoverageBanner from "../components/CoverageBanner";
-import JoinOurEvent from "../components/JoinOurEvent";
-import TestimonialsBanner from "../components/TestimonialsBanner";
-import SpecificationBanner from "../components/SpecificationBanner";
-import HealthBenefitsBanner from "../components/HealthBenefitsBanner";
-import RadiationImpactsBanner from "../components/RadiationImpactsBanner";
+import SEO from '../components/SEO';
+import CtaBanner from '../components/CtaBanner';
+import WelcomeBanner from '../components/WelcomeBanner';
+import PortableFeatureBanner from '../components/PortableFeatureBanner';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import DailyExposureBanner from '../components/2_DailyExposure';
+import SurroundingRadiationBanner from '../components/3_SurroundingRadiation';
+import CoverageBanner from '../components/CoverageBanner';
+import JoinOurEvent from '../components/JoinOurEvent';
+import TestimonialsBanner from '../components/TestimonialsBanner';
+import SpecificationBanner from '../components/SpecificationBanner';
+import HealthBenefitsBanner from '../components/HealthBenefitsBanner';
+import RadiationImpactsBanner from '../components/RadiationImpactsBanner';
+import SolutionBanner from '../components/SolutionBanner';
+
 export default function Home() {
-  return (
-    <>
-      <SEO />
-      <main>
-        <Navbar />
-        <WelcomeBanner />
-        <RadiationIntroBanner />
-        <RadiationImpactsBanner />
-        <SpecificationBanner />
-        <CoverageBanner />
-        <PortableFeatureBanner />
-        <HealthBenefitsBanner />
-        <TestimonialsBanner />
-        {/* <JoinOurEvent /> */}
-        <CtaBanner />
-        <Footer />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <SEO />
+            <main>
+                <Navbar />
+                {/* PROBLEM: Establish the threat and urgency */}
+                <WelcomeBanner />
+                {/* FEATURES: Additional product benefits */}
+                <DailyExposureBanner />
+
+                <SurroundingRadiationBanner />
+
+                {/* IMPACT: Show the health consequences */}
+                <RadiationImpactsBanner />
+
+                {/* SOLUTION: Introduce emGuarde as the answer */}
+                <SolutionBanner />
+
+                {/* BENEFITS: Show real-world transformation results */}
+                <HealthBenefitsBanner />
+
+                {/* PROOF: Social proof and testimonials */}
+                <TestimonialsBanner />
+
+                {/* PRODUCT DETAILS: Technical specs and coverage */}
+                <SpecificationBanner />
+                <CoverageBanner />
+                <PortableFeatureBanner />
+
+                {/* EVENT: Community engagement (commented out as requested in original) */}
+                {/* <JoinOurEvent /> */}
+
+                {/* CTA: Final conversion push */}
+                <CtaBanner />
+
+                <Footer />
+            </main>
+        </>
+    );
 }
